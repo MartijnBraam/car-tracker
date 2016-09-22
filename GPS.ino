7fecb735bc43eb06324ffddd7c5727a2eb19a863
Martijn Braam <martijn@brixit.nl>
2016-09-22 10:56:35 +0200

    Initial commit

---
+++ GPS.ino
@@ -0,0 +1,200 @@
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+#include <ESP8266WiFi.h>
+#include <SD.h>
+#include <SPI.h>
+
+SoftwareSerial GPS=SoftwareSerial(D1, D2);
+
+TinyGPSPlus nmea;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println();
+  Serial.println("Debugging started");
+
+
+  pinMode(D1, INPUT);
+  pinMode(D2, OUTPUT);
+  GPS.begin(9600);
+  Serial.println("SoftSerial started");
+
+  Serial.println("Starting SD");
+  if(SD.begin(SS)){
+    Serial.println("SD Init success");
+  }else{
+    Serial.println("SD Init failure");
+  }
+
+  File nameFile = SD.open("name.txt");
+  uint32_t nameSize = nameFile.size();
+  char trackerName[nameSize+1];
+  trackerName[nameSize] = '\0';
+  nameFile.read(trackerName, nameSize);
+  nameFile.close();
+  Serial.printf("Tracker: %s", trackerName);
+
+  Serial.println("Connecting to wifi network");
+  //connect_wifi();
+}
+
+void connect_wifi(){
+  // TODO: READ wifi data from SD
+  WiFi.begin("BrixIT Computers", "");
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(WiFi.localIP());
+}
+
+void fmtDouble(double val, byte precision, char *buf, unsigned bufLen = 0xffff);
+unsigned fmtUnsigned(unsigned long val, char *buf, unsigned bufLen = 0xffff, byte width = 0);
+
+void loop() {
+  Serial.println("Starting to parse nmea messages");
+  bool locked = false;
+  int lastSats = 0;
+  File logfile = SD.open("log.txt", FILE_WRITE);
+  logfile.seek(logfile.size()-1);
+  while(true){
+    delay(10);
+    while(GPS.available()){
+      if(nmea.encode(GPS.read())){
+          if(!locked && nmea.location.isValid()){
+            Serial.println("Got GPS fix");
+            locked = true;
+          }
+          if(locked && !nmea.location.isValid()){
+            Serial.println("Lost GPS fix");
+            locked = false;
+          }
+        
+          if(locked){
+            char lat[20];
+            char lng[20];
+            char speed[20];
+            fmtDouble(nmea.location.lat(), 10, lat);
+            fmtDouble(nmea.location.lng(), 10, lng);
+            fmtDouble(nmea.speed.kmph(),2, speed);
+            logfile.printf("%d,%d,%s,%s,%s\n", nmea.date.value(), nmea.time.value(), lat, lng, speed);
+            logfile.flush();
+          }else{
+            if(nmea.satellites.value() > lastSats){
+              lastSats = nmea.satellites.value();
+              Serial.printf("Got new sattelite. Now %d\n", lastSats);
+            }
+          }
+      }
+    }
+  }
+  logfile.close();
+}
+
+//
+// Produce a formatted string in a buffer corresponding to the value provided.
+// If the 'width' parameter is non-zero, the value will be padded with leading
+// zeroes to achieve the specified width.  The number of characters added to
+// the buffer (not including the null termination) is returned.
+//
+unsigned
+fmtUnsigned(unsigned long val, char *buf, unsigned bufLen, byte width)
+{
+ if (!buf || !bufLen)
+   return(0);
+
+ // produce the digit string (backwards in the digit buffer)
+ char dbuf[10];
+ unsigned idx = 0;
+ while (idx < sizeof(dbuf))
+ {
+   dbuf[idx++] = (val % 10) + '0';
+   if ((val /= 10) == 0)
+     break;
+ }
+
+ // copy the optional leading zeroes and digits to the target buffer
+ unsigned len = 0;
+ byte padding = (width > idx) ? width - idx : 0;
+ char c = '0';
+ while ((--bufLen > 0) && (idx || padding))
+ {
+   if (padding)
+     padding--;
+   else
+     c = dbuf[--idx];
+   *buf++ = c;
+   len++;
+ }
+
+ // add the null termination
+ *buf = '\0';
+ return(len);
+}
+
+//
+// Format a floating point value with number of decimal places.
+// The 'precision' parameter is a number from 0 to 6 indicating the desired decimal places.
+// The 'buf' parameter points to a buffer to receive the formatted string.  This must be
+// sufficiently large to contain the resulting string.  The buffer's length may be
+// optionally specified.  If it is given, the maximum length of the generated string
+// will be one less than the specified value.
+//
+// example: fmtDouble(3.1415, 2, buf); // produces 3.14 (two decimal places)
+//
+void
+fmtDouble(double val, byte precision, char *buf, unsigned bufLen)
+{
+ if (!buf || !bufLen)
+   return;
+
+ // limit the precision to the maximum allowed value
+ const byte maxPrecision = 6;
+ if (precision > maxPrecision)
+   precision = maxPrecision;
+
+ if (--bufLen > 0)
+ {
+   // check for a negative value
+   if (val < 0.0)
+   {
+     val = -val;
+     *buf = '-';
+     bufLen--;
+   }
+
+   // compute the rounding factor and fractional multiplier
+   double roundingFactor = 0.5;
+   unsigned long mult = 1;
+   for (byte i = 0; i < precision; i++)
+   {
+     roundingFactor /= 10.0;
+     mult *= 10;
+   }
+
+   if (bufLen > 0)
+   {
+     // apply the rounding factor
+     val += roundingFactor;
+
+     // add the integral portion to the buffer
+     unsigned len = fmtUnsigned((unsigned long)val, buf, bufLen);
+     buf += len;
+     bufLen -= len;
+   }
+
+   // handle the fractional portion
+   if ((precision > 0) && (bufLen > 0))
+   {
+     *buf++ = '.';
+     if (--bufLen > 0)
+       buf += fmtUnsigned((unsigned long)((val - (unsigned long)val) * mult), buf, bufLen, precision);
+   }
+ }
+
+ // null-terminate the string
+ *buf = '\0';
+}
+
